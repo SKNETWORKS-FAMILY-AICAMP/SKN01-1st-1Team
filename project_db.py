820ff38b18d159fb0d70fabf2b7b3be1c29d6a5b
hojoon <hojunlee1995@gmail.com>
2024-05-28 14:05:53 +0900

    240528_5

--- project_db.py
+++ project_db.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
         host = "localhost"
         port = 3306
         dbname = "project"
-        table = "review"
+        
+        table = "review" # 넣을 table 잘 설정해주기 <=> record_insert와 호환
+
         db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{dbname}"
-        record_insert(os.path.join(r"C:\Users\hojun\Downloads\workspace01\data\리뷰_원본.xlsx"), db_url, table)
-        #record_insert(os.path.join(r"C:\Users\hojun\Downloads\workspace01\data\케이카_원본.xlsx"))
-        #record_insert(os.path.join(r"C:\Users\hojun\Downloads\workspace01\data\FAQ_원본.xlsx"))
+        record_insert(os.path.join(os.getcwd() + "\\data\\리뷰.xlsx"), db_url, table)
+        #record_insert(os.path.join(os.getcwd() + "\\data\\케이카_원본.xlsx")db_url, table)
+        #record_insert(os.path.join(os.getcwd() + "\\data\\FAQ_원본.xlsx")db_url, table)
 
         # 파이썬과 db연동해서 crud 실행 
         #db = MySQLDatabase(db_url)
